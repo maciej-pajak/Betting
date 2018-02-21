93541232b04ab8828ed60f8e83b8185e89100548
maciej-pajak <maciek.pajak@vp.pl>
2018-02-21 15:57:18 +0100

    add comments

--- src/main/java/pl/maciejpajak/engine/SubscriptionMailGenerator.java
+++ src/main/java/pl/maciejpajak/engine/SubscriptionMailGenerator.java
@@ -7,12 +7,18 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import pl.maciejpajak.repository.CompetitionNewsRepository;
 import pl.maciejpajak.repository.UserRepository;
 
+/**
+ * Class handles sending emails with subscriptions.
+ * 
+ * @author mac
+ *
+ */
 @Component
 public class SubscriptionMailGenerator {
     
@@ -28,18 +34,19 @@ public class SubscriptionMailGenerator {
     private CompetitionNewsRepository competitionNewsRepository;
     
     /**
-     * Creates email messages with subscriptions and sends them to users.
+     * Creates email messages with subscriptions and sends them to users,
+     * every day at midnight.
      */
     @Transactional
-    @Async // for demonstration only, normally this should be @Scheduled, for instance once a night
+    @Scheduled(cron = "0 0 * * * *")
     public void sendSubscriptions() {
         userRepository.findAllByVisible(true).forEach(u -> {
             StringBuilder sb = new StringBuilder();
             competitionNewsRepository
                 .findAllBySentAndCompetitionCompetitionPreferencesUserIdAndCompetitionCompetitionPreferencesSubscribed(false, u.getId(), true)
-                .stream().forEach(n -> {
-                    sb.append(n.getContent()).append("\n");
-                });
+                .stream().forEach(n -> 
+                    sb.append(n.getContent()).append("\n")
+                );
             if (sb.length() != 0) {
                 sendSimpleMessage(u.getEmail(), "Subscription", sb.toString());
             }
@@ -48,6 +55,12 @@ public class SubscriptionMailGenerator {
                             .findAllBySent(false).stream().peek(n -> n.setSent(true)).collect(Collectors.toList()));
     }
     
+    /**
+     * Sends email {@code to} with {@code subject} and content {@code text}
+     * @param to - recipient email
+     * @param subject - message subject
+     * @param text - message content
+     */
     private void sendSimpleMessage (
             String to, String subject, String text) {
         log.debug("sending new message TO: {}\nSUBJECT: {}\nCONTENT: {}", to, subject, text);
