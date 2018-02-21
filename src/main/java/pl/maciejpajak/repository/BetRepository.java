41e8d62b275f29d370a52b90eae35d7c7837cf36
maciej-pajak <maciek.pajak@vp.pl>
2018-02-21 17:25:37 +0100

    add new method

--- src/main/java/pl/maciejpajak/repository/BetRepository.java
+++ src/main/java/pl/maciejpajak/repository/BetRepository.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import pl.maciejpajak.domain.bet.Bet;
 import pl.maciejpajak.domain.util.BetLastCall;
@@ -17,4 +20,10 @@ public interface BetRepository extends JpaRepository<Bet, Long> {
     public Collection<Bet> findAllByGameIdAndLastCallAndVisible(Long gameId, BetLastCall lastCall, boolean isVisible) ;
     
     public Collection<Bet> findAllByGameIdAndVisible(Long gameId, boolean isVisible);
+    
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Bet b SET b.betable = :betable WHERE b.game.id = :gameId AND b.visible = :isVisible")
+    public Integer updateBetableFlag(  @Param("betable") boolean betable,
+                                    @Param("gameId") Long gameId,
+                                    @Param("isVisible") boolean isVisible);
 }
